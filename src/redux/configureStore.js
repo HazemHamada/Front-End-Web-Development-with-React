9167759a9131d767a417a66165aa2ea8059022ec
Hazem Hamada <zoma_hamada@hotmail.com>
2020-10-03 21:43:21 +0200

    Add files via upload

---
+++ src/redux/configureStore.js
@@ -0,0 +1,69 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
+        applyMiddleware(thunk, logger)
+    );
+
+    return store;
+}   
+
+
+
+
+
+
+
+
+/*import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+    );
+
+    return store;
+}*/
+
+
+
+
+
+/*import {createStore} from 'redux';
+import { Reducer, initialState } from './reducer'
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        Reducer, // reducer
+        initialState, // our initialState
+    );
+
+    return store;
+}*/
